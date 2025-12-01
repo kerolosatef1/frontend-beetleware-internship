87d79c7ee43aec38db61cab0e17bc4fe6364c35e
kerolosatef1 <atefkerolos25@gmail.com>
2025-12-01 22:35:36 +0200

    initial commit

--- QA/QA/Template/TemplateName.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-//import Style from "./TemplateName.module.css";
-export default function TemplateName(){
-    const [count, setCount] = useState(0);
-    useEffect(() => {},[]);
-    return <>
-    <div className="text-green-500">TemplateName</div>
-    </>
-}
--- QA/QA/src/components/Data/Data.tsx
+++
@@ -1,9 +0,0 @@
-export const question: { id: number; question: string; answer: string }[] = [];
-
-export default function Data() {
-  return (
-    <>
-      <div className="text-green-500">Data</div>
-    </>
-  );
-}
